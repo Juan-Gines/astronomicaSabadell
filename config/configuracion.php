4d5ba1cf9b20e888a64b0fd5e6dd0311c4acfd0f
Juan-Gines <JuangAlenta@hotmail.com>
2021-10-20 08:30:57 +0200

    resultado devuelto correctamente

--- config/configuracion.php
+++ config/configuracion.php
@@ -2,11 +2,11 @@
 class Configuracion{
   protected const MONEDA='978';
   protected const CODIGO="999008881";
-  protected const URL= "http://localhost/cursophp/astronomicaSabadell/index.php";
+  protected const URL= "http://jgexperimentos.atwebpages.com/astronomicaSabadell/index.php";
   protected const TERMINAL='1';
   protected const TIPO='0';
-  protected const KO='0';
-  protected const OK='0';
+  protected const KO='http://localhost/cursophp/astronomicaSabadell/index.php';
+  protected const OK='http://localhost/cursophp/astronomicaSabadell/index.php';
   protected const NAME='Astronómica de Sabadell';
   protected const KC='sq7HjrUOBfKmC576ILgskD5srU870gJ7';
   protected const VERSION="HMAC_SHA256_V1";
